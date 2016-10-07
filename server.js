043c8b6ae4c38d2568f6ef7e4df63f5aa099bb40
aranga1 <aranga@purdue.du>
2016-10-07 02:53:17 -0400

    Heroku

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-const config = require('./config.js');
-const restify = require('restify');
-const builder = require('botbuilder');
-const server = restify.createServer().listen(8080);
+'use strict'
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = request('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+app.get('/', function( req, res) {
+	res.send('Hello world, I am a chat bot');
+});
+
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'alfred') {
+		res.send(req.query['hub.challenge']);
+	}
+	res.send('Error, wrong token');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'));
+});
+
