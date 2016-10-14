7430ddad1c6af0b84537421c7f26f52a822685da
Aakash Ranga <aranga@purdue.edu>
2016-10-14 15:13:04 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-	appid: '1813259925630355',
-	secret: 'b775c5b05dc4458dac8788ec47cb7772',
-	token: 'EAAZAxJnnArZAMBABBmuzZBPpU8ekoGLMezrTzIiZB3qCwMeCrPB5225KAXwk3oqkL5WZCm45ZA6Hc77hV3WTt1CkB16MGbx32Dgp28wpylVKgLx9UCuWpRgQ1vygfnQqq16Cq7LluTGIZAyc4QmynGRtYKJWZCRO6D2B7ZBZCVwCSfjwZDZD',
-	recast_token: '1f4ae8a746878df6679216a256212ac3',
-}
-
-module.exports = config;
